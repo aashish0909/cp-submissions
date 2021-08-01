36a51f4a9511549fd6129830cb566299e1b1771c
Aashish Illa <illa.aashish999@gmail.com>
2021-08-01 19:32:00 +0530

    Add solution for problem `A - Alloy` Link: https://atcoder.jp/contests/abc212/tasks/abc212_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc212/submissions/24720626

---
+++ atcoder/abc212/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	//read the question properly
+	int a, b;
+	cin >> a >> b;
+	if (!a) cout << "Silver";
+	else if (!b) cout << "Gold";
+	else cout << "Alloy";
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	//cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
