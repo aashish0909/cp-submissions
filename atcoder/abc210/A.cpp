e9f2a966566d980358ea99b84407639695c218fe
Aashish Illa <illa.aashish999@gmail.com>
2021-08-02 15:25:00 +0530

    Add solution for problem `A - Cabbages` Link: https://atcoder.jp/contests/abc210/tasks/abc210_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc210/submissions/24732981

---
+++ atcoder/abc210/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	//read the question properly
+	int n, a, x, y;
+	cin >> n >> a >> x >> y;
+
+	cout << min(n, a)*x + max(0ll, (n - a)*y);
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	//cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
